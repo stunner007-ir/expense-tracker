f517726234c1bd02edcebf4f6ed43b7e9ad27931
Ishu Raj <ishu9693raj@gmail.com>
2024-05-18 18:20:32 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={"Car Insurance"}
+        amount={50}
+        date={new Date(2023, 2, 28)}
+      ></ExpenseItem>
     </div>
   );
 }
